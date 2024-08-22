46c1e8b79838edad88594628a2ab155d870a4d54
gongbu22 <pyujin0711@naver.com>
2024-08-22 10:51:36 +0900

    로그인 처리를 위한 login_member 작성

--- app/service/member.py
+++ app/service/member.py
@@ -1,5 +1,5 @@
 import requests
-from sqlalchemy import insert
+from sqlalchemy import insert, select, and_
 from sqlalchemy.exc import SQLAlchemyError
 
 from app.model.member import Member
@@ -26,7 +26,7 @@ class MemberService:
     @staticmethod
     def check_captcha(member):
         req_url = 'https://www.google.com/recaptcha/api/siteverify'
-        params = { 'secret': '',# 비밀키 - github 올리지 말기
+        params = { 'secret': '6LePoCsqAAAAAMNqzyue_IKTOqknBi2CM_KLB4gd',# 비밀키 - github 올리지 말기
                    'response': member.captcha }
         res = requests.get(req_url, params=params)
         result = res.json()
@@ -35,3 +35,14 @@ class MemberService:
         return result['success']
         # return True
 
+    @staticmethod
+    def login_member(db, data):
+        try:
+            find_login = and_(Member.userid == data.get('userid'),
+                              Member.passwd == data.get('passwd'))
+            stmt = select(Member.userid).where(find_login)
+            result = db.execute(stmt).scalars().first()
+
+            return result
+        except SQLAlchemyError as ex:
+            print(f'▶▶▶ login_member 오류 발생: {str(ex)}')
